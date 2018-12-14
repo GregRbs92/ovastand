7d34108c9e507348144dbb84f32d8a8a1f9e586c
tbabinet <tbabinet@hotmail.fr>
2018-12-14 16:13:26 +0100

    modal ok, media queries pour régler l'affichage mais ça marche pas, baisez tous vos mères, fuck les QM, fuck le webdev, fuck moi

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -52,6 +52,7 @@ import { forkJoin } from 'rxjs/observable/forkJoin';
 
 export class HomepageComponent implements OnInit {
 
+   
     option: number = 1;
     hidePortfolio: boolean = null;
     scrolled: boolean = false;
@@ -60,7 +61,7 @@ export class HomepageComponent implements OnInit {
     albums: Album[];
     collabs: Collaborateur[];
     artisteFocused: Artiste;
-    albumFocused: Album; 
+    albumFocused: Album;
     descriptionOnFocus: string[];
     datesOnFocus: string[];
     connectionError: boolean = false;
@@ -68,89 +69,90 @@ export class HomepageComponent implements OnInit {
         main_color: "#ed6f7d",
         second_color: "#333"
     };
-    safeYtUrl1; safeYtUrl2; safeSpotifyUrl; safeDeezerUrl : SafeResourceUrl;
+    safeYtUrl1; safeYtUrl2; safeSpotifyUrl; safeDeezerUrl: SafeResourceUrl;
 
 
     logo: string = "../../../assets/logo-noir.png";
 
     partenaires: Partenaire[];
 
-  constructor(private http:HttpClient, private collabProvider:CollaborateursService, private artisteProvider:ArtistesService,private albumProvider:AlbumsService, private partenaireProvider: PartenairesService, private fs:FileService, private sanitizer : DomSanitizer) {
-  }
-
-  ngOnInit() {
-
-    forkJoin(
-      this.artisteProvider.getArtistes(),
-      this.albumProvider.getAlbums(),
-      this.partenaireProvider.getPartenaires(),
-      this.collabProvider.getCollabs(),
-      this.fs.getColors()
-    )
-    .subscribe(res => {
-      this.connectionError = false;
-      this.artistes = res[0] ? res[0].sort(this.compareOrder) : [];
-      this.albums = res[1] ? res[1] : [];
-      this.partenaires = res[2] ? res[2] : [];
-      this.collabs = res[3] ? res[3] : [];
-      this.colors = res[4];
-    }, err => {
-      console.error(err);
-      this.connectionError = true;
-    });
-
-  }
-
-  @HostListener('window:scroll')
-  onScroll() {
-    if(!this.scrolled && window.pageYOffset > 100) {
-        this.scrolled = true;
-        this.logo = "../../../assets/logo.png";
+    constructor(private http: HttpClient, private collabProvider: CollaborateursService, private artisteProvider: ArtistesService, private albumProvider: AlbumsService, private partenaireProvider: PartenairesService, private fs: FileService, private sanitizer: DomSanitizer) {
     }
-    else if(this.scrolled && window.pageYOffset <= 100) {
-        this.scrolled = false;
-        this.logo = "../../../assets/logo-noir.png";
+
+    ngOnInit() {
+
+        forkJoin(
+            this.artisteProvider.getArtistes(),
+            this.albumProvider.getAlbums(),
+            this.partenaireProvider.getPartenaires(),
+            this.collabProvider.getCollabs(),
+            this.fs.getColors()
+        )
+            .subscribe(res => {
+                this.connectionError = false;
+                this.artistes = res[0] ? res[0].sort(this.compareOrder) : [];
+                this.albums = res[1] ? res[1] : [];
+                this.partenaires = res[2] ? res[2] : [];
+                this.collabs = res[3] ? res[3] : [];
+                this.colors = res[4];
+
+            }, err => {
+                console.error(err);
+                this.connectionError = true;
+            });
+        
+
     }
-  }
 
-  selectArtiste(artiste: Artiste) {
-      this.artisteFocused = artiste;
-      this.descriptionOnFocus = artiste.description.split('\n');
-      this.safeYtUrl1 = this.sanitizeUrl(artiste.videoUrl1);
-      this.safeYtUrl2 = this.sanitizeUrl(artiste.videoUrl2);
-  }
-
-  selectAlbum(album : Album) {
-      this.albumFocused = album;
-      this.safeSpotifyUrl = this.sanitizeUrl(album.spotify);
-      this.safeDeezerUrl = this.sanitizeUrl(album.deezer);
-  }
-
-  closeArtistePortfolio(){
-    this.artisteFocused = null;
-  }
-
-  closeAlbumPortfolio(){
-    this.albumFocused = null;
-  }
-
-  sanitizeUrl(url){
-    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
-  }
-
-  setOption(page: number) {
-    this.option = page;
-  }
-
-  compareOrder(art1, art2) {
-    if (art1.ordre > art2.ordre) {
-      return 1;
+    @HostListener('window:scroll')
+    onScroll() {
+        if (!this.scrolled && window.pageYOffset > 100) {
+            this.scrolled = true;
+            this.logo = "../../../assets/logo.png";
+        }
+        else if (this.scrolled && window.pageYOffset <= 100) {
+            this.scrolled = false;
+            this.logo = "../../../assets/logo-noir.png";
+        }
     }
-    if (art1.ordre < art2.ordre) {
-      return -1;
+
+    selectArtiste(artiste: Artiste) {
+        this.artisteFocused = artiste;
+        this.descriptionOnFocus = artiste.description.split('\n');
+        this.safeYtUrl1 = this.sanitizeUrl(artiste.videoUrl1);
+        this.safeYtUrl2 = this.sanitizeUrl(artiste.videoUrl2);
     }
-    return 0;
-  }
+
+    selectAlbum(album: Album) {
+        this.albumFocused = album;
+        this.safeSpotifyUrl = this.sanitizeUrl(album.spotify);
+        this.safeDeezerUrl = this.sanitizeUrl(album.deezer);
+    }
+
+    closeArtistePortfolio() {
+        this.artisteFocused = null;
+    }
+
+    closeAlbumPortfolio() {
+        this.albumFocused = null;
+    }
+
+    sanitizeUrl(url) {
+        return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+    }
+
+    setOption(page: number) {
+        this.option = page;
+    }
+
+    compareOrder(art1, art2) {
+        if (art1.ordre > art2.ordre) {
+            return 1;
+        }
+        if (art1.ordre < art2.ordre) {
+            return -1;
+        }
+        return 0;
+    }
+
 }
-
-
